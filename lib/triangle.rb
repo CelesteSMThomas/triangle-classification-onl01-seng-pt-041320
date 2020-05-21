92d8ecdcea459db5c04b7761ee3e13f10ffcbecb
celestesmthomas <celestesmthomas@gmail.com>
2020-05-21 13:00:41 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,7 @@ class Triangle
         (length_one != length_two) && (length_one != length_three) && (length_two != length_three)
         return :scalene
         binding.pry
-      elsif ((length_one + length_two) < length_three) || ((length_one + length_three) < length_two) || ((length_two + length_three) < length_one) 
+      elsif ((length_one + length_two) >= length_three) || ((length_one + length_three) >= length_two) || ((length_two + length_three) >= length_one) 
         raise TriangleError
     end
 
