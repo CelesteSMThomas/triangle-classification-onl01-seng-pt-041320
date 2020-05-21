b0098d551ecea085f2ad220bbd7e3452dc6d82b9
celestesmthomas <celestesmthomas@gmail.com>
2020-05-21 13:19:11 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,8 @@ class Triangle
     def kind
       if length_one <= 0 || length_two <= 0 || length_three <= 0
         raise TriangleError
+      elsif ((length_one + length_two) <= length_three) || ((length_one + length_three) <= length_two) || ((length_two + length_three) <= length_one) 
+        raise TriangleError
       elsif length_one == length_two && length_one == length_three
           return :equilateral
       elsif
@@ -20,9 +22,7 @@ class Triangle
       elsif
         (length_one != length_two) && (length_one != length_three) && (length_two != length_three)
         return :scalene
-        binding.pry
-      elsif ((length_one + length_two) >= length_three) || ((length_one + length_three) >= length_two) || ((length_two + length_three) >= length_one) 
-        raise TriangleError
+        #binding.pry
     end
 
   end
